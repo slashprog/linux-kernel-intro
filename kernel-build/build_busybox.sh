035012cd9c4d9437a7effbc028a9df2b68ea7dac
Chandrashekar Babu <training@chandrashekar.info>
2024-08-21 12:53:08 +0530

    Created automated build scripts for busybox

---
+++ kernel-build/build_busybox.sh
@@ -0,0 +1,38 @@
+#!/bin/sh
+
+# Building busybox using preset configuration
+
+BUSYBOX_VERSION=1.36.1
+BUSYBOX_BUILD_SUFFIX=vbox
+
+BUSYBOX_RELEASE="${BUSYBOX_VERSION}-${BUSYBOX_BUILD_SUFFIX}"
+
+BUSYBOX_DOWNLOAD_URL="https://busybox.net/downloads/busybox-S{BUSYBOX_VERSION}.tar.bz2"
+BUSYBOX_PRESET_CONFIG_URL="https://raw.githubusercontent.com/slashprog/linux-kernel-intro/main/kernel-build/busybox-config-vbox"
+
+WORK_FOLDER=$HOME
+SOURCE_FOLDER=/usr/local/src/busybox-${BUSYBOX_VERSION}/
+BUILD_FOLDER=/usr/local/build/busybox-${BUSYBOX_RELEASE}/
+
+### Step 1: Prepare the build environment
+cd $WORK_FOLDER
+curl -O $BUSYBOX_DOWNLOAD_URL
+
+tar xvf busybox-${BUSYBOX_VERSION}.tar.bz2 -C /usr/local/src
+chattr -Rf +i $SOURCE_FOLDER
+
+mkdir -p $BUILD_FOLDER
+curl -o ${BUILD_FOLDER}/.config $BUSYBOX_PRESET_CONFIG_URL
+
+#### Step 2: Configure the build
+cd $SOURCE_FOLDER
+make O=${BUILD_FOLDER} oldconfig 
+#make O=${BUILD_FOLDER} menuconfig
+
+#### Step 3: Build
+make
+
+#### Step 4: Install (by default under $BUILD_FOLDER/_install)
+make install
+
+
---
+++ kernel-build/create_custom_disk_image.sh
@@ -0,0 +1,78 @@
+#!/bin/sh
+
+# Create a new disk image with custom kernel build with busybox as user-land
+# ---------------------------------------------------------------------------
+
+# You should notice existence of a new hard drive labeled sdb
+# when you run this command on our VirtualBox VM setup.
+lsblk
+
+# Create a DOS MBR disklabel (partition format)
+parted -s /dev/sdb mklabel msdos
+
+# Create a new partition on this hard drive
+parted -s /dev/sdb mpart primary ext4 0% 100%
+
+# Verify the partition name shown as sdb1
+lsblk
+
+# Format this partition (sdb1) using Ext4 filesystem format.
+mkfs.ext4 -F /dev/sdb1
+
+# Mount the newly formatted partition into /mnt
+mount /dev/sdb1 /mnt
+
+# Install the kernel tarball of our custom kernel build into this
+# mounted partition
+cd /mnt
+tar xvf /usr/local/build/linux-6.10.6-vbox/linux-6.10.6-vbox-x86.tar.xz
+
+# Install the GRUB boot loader on this hard drive - where stage1 of
+# the boot loader would reside in the MBR (first sector of the hard drive)
+# and grub modules would reside in the /boot/grub/ folder on this
+# hard drive's first partition (mounted on /mnt/)
+grub-install --boot-directory=/mnt/boot /dev/sdb
+
+# Create a simple GRUB configuration file to boot our kernel.
+cat > /mnt/boot/grub/grub.cfg <<END_GRUB_CONFIG
+menuentry "Boot Linux 6.10.6-vbox with BusyBox" {
+    linux (hd1,1)/boot/vmlinuz-6.10.6-vbox root=/dev/sdb1 rw
+}
+END_GRUB_CONFIG
+
+# Install BusyBox user-land tools
+cp -a /usr/local/build/busybox-1.36.1-vbox/_install/* /mnt/
+
+# Create important top-level folders that are crucial for
+# mounting - proc, sysfs, devtmpfs and tmpfs pseudo-filesystems
+cd /mnt
+mkdir proc sys dev tmp
+
+# The following folders are optional, but good to have them created.
+mkdir etc etc var root mnt media run srv
+
+# Busybox init program expects rcS script to be in /etc/init.d/rcS
+mkdir -p etc/init.d
+
+# Some var subfolders that might be used by certain services/daemons
+mkdir -p var/log var/lib var/local var/cache var/spool var/mail
+ln -s ../run var/run
+ln -s ../tmp var/tmp
+
+# Create a basic sysinit script that will be run by BusyBox init
+cat > /mnt/etc/init.d/rcS << END_RCS
+#!/bin/sh
+
+mount proc /proc -t proc
+mount sysfs /sys -t sysfs
+mount tmpfs /tmp -t tmpfs
+mkdir -p /dev/pts
+mount devpts /dev/pts -t devpts
+
+# Launch any custom service / application by adding commands below:
+echo "Linux system with BusyBox is ready."
+
+END_RCS
+
+chmod +x /mnt/etc/init.d/rcS
+
