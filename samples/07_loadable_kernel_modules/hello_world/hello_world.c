10cfae5272fed4f1086256138dd581effa502db3
Linux Hacker <root@archvm.local>
2024-09-10 10:27:13 +0530

    Demonstration of diagnostics / features
    In this commit, I demonstrate the use of __init and
    __exit annotations, and the use of dump_stack()
    and msleep_interruptible()

--- samples/07_loadable_kernel_modules/hello_world/hello_world.c
+++ samples/07_loadable_kernel_modules/hello_world/hello_world.c
@@ -1,22 +1,31 @@
 #include <linux/module.h>
 #include <linux/kernel.h>
+#include <linux/delay.h>
 
-static int hello_world_init(void)
+char hello_msg[] __initdata = "Hello, world - from kernel-space.";
+
+static int __init hello_world_init(void)
 {
-	pr_notice("%s:[%s:%d]:%s: Hello, world - from kernel-space.\n",
-               KBUILD_MODNAME, __FILE__, __LINE__, __func__);
+	pr_notice("%s:[%s:%d]:%s: %s\n",
+               KBUILD_MODNAME, __FILE__, __LINE__, __func__, hello_msg);
 
-	/*
-	* printk(KERN_NOTICE "Hello world");
-	* printk("<5>Hello world");
-	*/
+	dump_stack();
+/* 	msleep_interruptible(30000);
+ */
+	pr_info("%s:%s: returning out.\n", KBUILD_MODNAME, __func__);
+
 	return 0;
 }
 
-static void hello_world_exit(void)
+static void __exit hello_world_exit(void)
 {
-	pr_info("%s:[%s:%d]:%s: Hello, world - from kernel-space.\n",
+	pr_info("%s:[%s:%d]:%s: Goodbye, cruel world.\n",
                KBUILD_MODNAME, __FILE__, __LINE__, __func__);
+
+	dump_stack();
+/* 	msleep_interruptible(30000);
+ */
+	pr_info("%s:%s: returning out.\n", KBUILD_MODNAME, __func__);
 }
 
 module_init(hello_world_init);
@@ -26,16 +35,3 @@ MODULE_LICENSE("GPL");
 
 MODULE_DESCRIPTION("A simple hello-world module.");
 MODULE_AUTHOR("Chandrashekar Babu <chandra@slashprog.com>");
-
-
-/*  Try out the following changes:
-*     1. Try returning a negative number from hello_world_init()
-*     1a. Try returning a positive number from hello_world_init()
-*
-*     2. Try changing the MODULE_LICENSE() from "GPL" to any other string
-*        like "Proprietary"
-*
-*     3. Try removing module_exit(), build, insert, remove and test.
-*
-*
-*   */
